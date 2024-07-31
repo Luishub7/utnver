436929d4afd767ae5dfab9b53a6b50433773da52
luis.dev <luisro43@gmail.com>
2024-07-31 08:48:46 -0300

    nuevos archivos

---
+++ src/components/ContactList.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const ContactList = ({ contacts }) => {
+  return (
+    <div className="contact-list">
+      {contacts.map(contact => (
+        <div key={contact.id} className="contact-item">
+          <img src={`/images/${contact.avatar}`} alt={contact.nombre} className="avatar" />
+          <div className="contact-info">
+            <h3>{contact.nombre}</h3>
+            <p>{contact.estadoMensaje}</p>
+          </div>
+          <Link to={`/chat/${contact.id}`} className="chat-link">
+            <button>Chat</button>
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ContactList;
