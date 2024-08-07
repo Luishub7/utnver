9bd59194b1a02d353bf066e1242a9fe16ca3aae2
luis.dev <luisro43@gmail.com>
2024-08-07 08:44:37 -0300

    images

--- src/data/contacts.js
+++ src/data/contacts.js
@@ -1,13 +1,13 @@
 // src/data/contacts.js
 export const CONTACTS = [
-  { id: 1, name: 'Pepe', avatar: '/src/imagenes/1.webp' },
-  { id: 2, name: 'Ana', avatar: '/src/imagenes/2.webp' },
-  { id: 3, name: 'Luis', avatar: '/src/imagenes/3.webp' },
-  { id: 4, name: 'María', avatar: '/src/imagenes/4.webp' },
-  { id: 5, name: 'Carlos', avatar: '/src/imagenes/5.webp' },
-  { id: 6, name: 'Laura', avatar: '/src/imagenes/6.webp' },
-  { id: 7, name: 'José', avatar: '/src/imagenes/7.webp' },
-  { id: 8, name: 'Elena', avatar: '/src/imagenes/8.webp' },
-  { id: 9, name: 'Fernando', avatar: '/src/imagenes/9.webp' },
-  { id: 10, name: 'Clara', avatar: '/src/imagenes/10.webp' },
+  { id: 1, name: 'Pepe', avatar: '/imagenes/1.webp' },
+  { id: 2, name: 'Ana', avatar: '/imagenes/2.webp' },
+  { id: 3, name: 'Luis', avatar: '/imagenes/3.webp' },
+  { id: 4, name: 'María', avatar: '/imagenes/4.webp' },
+  { id: 5, name: 'Carlos', avatar: '/imagenes/5.webp' },
+  { id: 6, name: 'Laura', avatar: '/imagenes/6.webp' },
+  { id: 7, name: 'José', avatar: '/imagenes/7.webp' },
+  { id: 8, name: 'Elena', avatar: '/imagenes/8.webp' },
+  { id: 9, name: 'Fernando', avatar: '/imagenes/9.webp' },
+  { id: 10, name: 'Clara', avatar: '/imagenes/10.webp' },
 ];
